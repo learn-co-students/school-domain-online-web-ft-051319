4483c261dc4097efa7e9f8d1c5f5f1b174c74121
Haley DeKeyser <hcd09@live.com>
2019-05-21 00:18:08 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name, roster = {})
+    @name = name
+    @roster = roster
+  end
+
+  def add_student(student, grade)
+    if roster.has_key?(grade)
+      roster[grade] << student
+    else
+      roster[grade] = []
+      roster[grade] << student
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each do |grades, students|
+      students.sort!
+    end
+  end
+
+end
